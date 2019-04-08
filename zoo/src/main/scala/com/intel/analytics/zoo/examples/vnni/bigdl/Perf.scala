b03375dd40ec4cb19a7c3853fdab4e58dc834631
hkvision <huangkaivision@gmail.com>
2019-04-08 16:47:45 +0800

    update

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/vnni/bigdl/Perf.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/vnni/bigdl/Perf.scala
@@ -19,26 +19,19 @@ package com.intel.analytics.zoo.examples.vnni.bigdl
 import com.intel.analytics.bigdl.nn._
 import com.intel.analytics.bigdl.numeric.NumericFloat
 import com.intel.analytics.bigdl.tensor.Tensor
-import com.intel.analytics.bigdl.utils.Engine
-import com.intel.analytics.zoo.common.NNContext
 import org.apache.log4j.Logger
 import scopt.OptionParser
 
 
 case class ResNet50PerfParams(model: String = "",
-                              batchSize: Int = 16,
+                              batchSize: Int = 32,
                               iteration: Int = 2000)
 
 object Perf {
 
   val logger: Logger = Logger.getLogger(getClass)
 
-  def main(argv: Array[String]): Unit = {
-    System.setProperty("bigdl.mkldnn.fusion.convbn", "true")
-    System.setProperty("bigdl.mkldnn.fusion.bnrelu", "true")
-    System.setProperty("bigdl.mkldnn.fusion.convrelu", "true")
-    System.setProperty("bigdl.mkldnn.fusion.convsum", "true")
-
+  def main(args: Array[String]): Unit = {
     System.setProperty("bigdl.localMode", "true")
     System.setProperty("bigdl.engineType", "mkldnn")
 
@@ -55,28 +48,26 @@ object Perf {
         .action((v, p) => p.copy(iteration = v))
     }
 
-    parser.parse(argv, ResNet50PerfParams()).foreach { params =>
-//      val sc = NNContext.initNNContext("Int8 Performance Test")
-
+    parser.parse(args, ResNet50PerfParams()).foreach { params =>
       val batchSize = params.batchSize
       val inputShape = Array(batchSize, 3, 224, 224)
       val input = Tensor(inputShape).rand()
 
+      // fp32 model running on MKLDNN
+//      val fp32model = Module.loadModule[Float](params.model) // .quantize()
+//      val model = ConversionUtils.convert[Float](fp32model)
+//      model.evaluate()
+
       val model = Module.loadModule[Float](params.model).quantize()
-      model.evaluate()
 
       var iteration = 0
       while (iteration < params.iteration) {
         val start = System.nanoTime()
-//        val output = model.forward(input)
-        Engine.dnnComputing.invokeAndWait2(Array(1).map(_ => () => {
-          val output = model.forward(input)
-        }))
+        model.forward(input)
+        val timeUsed = System.nanoTime() - start
 
-        val takes = System.nanoTime() - start
-
-        val throughput = "%.2f".format(batchSize.toFloat / (takes / 1e9))
-        logger.info(s"Iteration $iteration, takes $takes s, throughput is $throughput imgs/sec")
+        val throughput = "%.2f".format(batchSize.toFloat / (timeUsed / 1e9))
+        logger.info(s"Iteration $iteration, takes $timeUsed ns, throughput is $throughput imgs/sec")
 
         iteration += 1
       }
